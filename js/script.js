af29b6e488c9636aa056df42e23eebc86e4bb9f6
brcdev-back <brcdesenvolvedor@gmail.com>
2025-12-15 17:51:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,41 @@ let quantidade = 1;
 const chavePix = "81991610473";
 const numero = "5581991610473";
 
+
+const produtos = {
+  completo: {
+    preco: 5.00,
+    qtd: 0
+  },
+  simples: {
+    preco: 4.00,
+    qtd: 0
+  }
+};
+
+function alterarQtd(tipo, valor) {
+  produtos[tipo].qtd += valor;
+  if (produtos[tipo].qtd < 0) produtos[tipo].qtd = 0;
+
+  document.getElementById(`qtd-${tipo}`).innerText = produtos[tipo].qtd;
+  atualizarTotais();
+}
+
+function atualizarTotais() {
+  let totalGeral = 0;
+
+  for (let tipo in produtos) {
+    const total = produtos[tipo].qtd * produtos[tipo].preco;
+    totalGeral += total;
+
+    document.getElementById(`total-${tipo}`).innerText =
+      `Total: R$ ${total.toFixed(2).replace(".", ",")}`;
+  }
+
+  document.getElementById("total-geral").innerText =
+    `Total do pedido: R$ ${totalGeral.toFixed(2).replace(".", ",")}`;
+}
+
 function alterarQtd(valor) {
   quantidade += valor;
   if (quantidade < 1) quantidade = 1;
@@ -28,8 +63,12 @@ function pedido(tipo, id) {
   const msg =
 `PEDIDO - CALDINHO DE FEIJÃO
 
-Tipo: ${tipo}
-Quantidade: ${quantidade}
+ if (produtos.completo.qtd > 0) {
+    mensagem += `• Feijão Completo: ${produtos.completo.qtd}\n`;
+  }
+  if (produtos.simples.qtd > 0) {
+    mensagem += `• Feijão sem Charque: ${produtos.simples.qtd}\n`;
+  }
 
 Bloco: ${bloco}
 Apartamento: ${apto}
