f30cf538a804ce6dbb3ca6f63c6d70468c697079
brcdev-back <brcdesenvolvedor@gmail.com>
2025-12-15 17:01:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function pedido(tipo) {
   const numero = "5581991610473";
-  const mensagem = `Olá! Gostaria de pedir um caldinho de feijão (${tipo}).`;
+  const mensagem =
+    `Olá! Quero pedir um caldinho de feijão (${tipo}).\n` +
+    `Bloco:\nApartamento:\nForma de pagamento (Pix):`;
+    
   const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
-
   window.open(link, "_blank");
 }
