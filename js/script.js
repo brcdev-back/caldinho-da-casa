f6107c94495608978ae297b0c7e46cca9b394300
brcdev-back <brcdesenvolvedor@gmail.com>
2025-12-15 17:16:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 function pedido(tipo) {
   const numero = "5581991610473";
+
+  const bloco = document.getElementById("bloco").value;
+  const apartamento = document.getElementById("apartamento").value;
+
+  if (bloco === "" || apartamento === "") {
+    alert("Por favor, informe o bloco e o apartamento.");
+    return;
+  }
+
   const mensagem =
-    `Olá! Quero pedir um caldinho de feijão (${tipo}).\n` +
-    `Bloco:\nApartamento:\nForma de pagamento (Pix):`;
-    
+`PEDIDO – CALDINHO DE FEIJÃO
+ Tipo: ${tipo}
+ 
+ Bloco: ${bloco}
+ Apartamento: ${apartamento}
+
+ Forma de pagamento: Pix
+`;
+
   const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
   window.open(link, "_blank");
 }
